575491cbaa3eba517f365a811054fba2e5c89541
unknown <anirbankhan77@gmail.com>
2026-04-25 14:44:24 +0530

    second version of code

--- mycode.py
+++ mycode.py
@@ -4,8 +4,10 @@ import os
 data =  {'Name': ['John', 'Alice', 'Bob'],
          'Age': [25, 30, 22],
          'City': ['New York', 'Los Angeles', 'Chicago']}
-
+# Adding new row to the df
+new_row = {'Name':'Ani', 'Age':32, 'City':'Kolkata'}
 df = pd.DataFrame(data)
+df.loc[len(df.index)] = new_row
 data_dir = 'data'
 os.makedirs(data_dir, exist_ok=True)
 file_path = os.path.join(data_dir, 'mydata.csv')
