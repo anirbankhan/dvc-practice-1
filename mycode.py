735266e4317358ca481b7289aefbe563f00a3e28
unknown <anirbankhan77@gmail.com>
2026-04-25 13:04:54 +0530

    first version

--- mycode.py
+++ mycode.py
@@ -9,5 +9,6 @@ df = pd.DataFrame(data)
 data_dir = 'data'
 os.makedirs(data_dir, exist_ok=True)
 file_path = os.path.join(data_dir, 'mydata.csv')
+# uploading data into csv file
 df.to_csv(file_path, index=False)
 print (df)
