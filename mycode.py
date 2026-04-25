7464e26febcea8eba8971d439dd59cab51e9774b
unknown <anirbankhan77@gmail.com>
2026-04-25 12:58:19 +0530

    updated code repo

---
+++ mycode.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import os
+# sample data frame with column
+data =  {'Name': ['John', 'Alice', 'Bob'],
+         'Age': [25, 30, 22],
+         'City': ['New York', 'Los Angeles', 'Chicago']}
+
+df = pd.DataFrame(data)
+data_dir = 'data'
+os.makedirs(data_dir, exist_ok=True)
+file_path = os.path.join(data_dir, 'mydata.csv')
+df.to_csv(file_path, index=False)
+print (df)
